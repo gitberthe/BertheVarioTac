4ff7017adb19b8c610cd579712ec0d00fb882e90
SpinBerthe <spinberthe@gmail.com>
2024-09-20 16:11:24 +0200

    creation du premier git source

---
+++ BertheVarioTacPlatformIO/src/BertheVarioTac.cpp
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \file BertheVarioTac.cpp
+///
+/// \brief Main du projet de vario tactile
+///
+/// \date creation     : 20/09/2024
+/// \date modification : 20/09/2024
+///
+
+#include "BertheVarioTac.h"
+
+char g_NumVersion[] = "20240920a" ;
+
+////////////////////////////////////////////////////////////////////////////////
+/// \brief
+void setup()
+{
+// put your setup code here, to run once:
+Serial.begin(115200) ;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// \brief
+void loop()
+{
+// put your main code here, to run repeatedly:
+Serial.println("hello from ESP32-2432S028") ;
+}
