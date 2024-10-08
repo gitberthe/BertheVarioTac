d7d6923dceae9c3b32318ae6683a38d868140591
SpinBerthe <spinberthe@gmail.com>
2024-10-08 05:29:48 +0200

    modif pcb

--- BertheVarioTacPlatformIO/src/BertheVarioTac.cpp
+++ BertheVarioTacPlatformIO/src/BertheVarioTac.cpp
@@ -9,7 +9,7 @@
 
 #include "BertheVarioTac.h"
 
-char g_NumVersion[] = "20241007b" ;
+char g_NumVersion[] = "20241008a" ;
 
 ////////////////////////////////////////////////////////////////////////////////
 /// \brief Affiche le nom et le firmware
